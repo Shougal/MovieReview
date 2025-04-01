2af9bd3031c684f7de35518be38f93568ee71263
Rob <157063279+Rob-Keys@users.noreply.github.com>
2025-03-31 22:33:08 -0400

    Users can change pfp

--- private/sprint3/MovieReviewController.php
+++ private/sprint3/MovieReviewController.php
@@ -12,7 +12,7 @@ class MovieReviewController {
         $this->db = new Database();
         $this->input=$input;
 
-        
+
     }
 
     public function run() {
@@ -47,11 +47,36 @@ class MovieReviewController {
                 break;
             case "db-destroy":
                 $this->destroyDB();
+                break;
+            case "set-pfp":
+                $this->changePfp();
             case "home":
             default:
                 $this->showHome();
                 break;
         }
+    }
+    function changePfp(){
+        if(!isset($_POST["choice"])){
+            $this->showAccount();
+            return;
+        }
+        $pfp_choice = (int) $_POST["choice"];
+        $statement = "UPDATE users_spr3 SET pfp = ".$pfp_choice." WHERE email = '".$_SESSION["email"]."';";
+        $this->db->query($statement);
+        unset($statement);
+        switch($pfp_choice){
+            case "1":
+                $_SESSION["pfp"] = "bluepfp.jpg";
+                break;
+            case "2":
+                $_SESSION["pfp"] = "greenpfp.jpg";
+                break;
+            case "3":
+                $_SESSION["pfp"] = "orangepfp.jpg";
+                break;
+        }
+        $this->showAccount();
     }
     function destroyDB() {
         $this->db->dropTables();
@@ -68,7 +93,7 @@ class MovieReviewController {
     function showUserMovies(){
         // include("/students/xdq9qa/students/xdq9qa/private/sprint3/templates/userMovies.php");
         include("/opt/src/sprint3/templates/userMovies.php");
-        
+
     }
     function showRecommendations(){
         // include("/students/xdq9qa/students/xdq9qa/private/sprint3/templates/userRecommendation.php");
@@ -77,12 +102,12 @@ class MovieReviewController {
     function showReview(){
         // include("/students/xdq9qa/students/xdq9qa/private/sprint3/templates/review.php");
         include("/opt/src/sprint3/templates/review.php");
-        
+
     }
     function showHome(){
         // include("/students/xdq9qa/students/xdq9qa/private/sprint3/templates/home.php");
         include("/opt/src/sprint3/templates/home.php");
-        
+
     }
 
     /** This shows all movies searched by the user based on query inputted */
@@ -92,7 +117,7 @@ class MovieReviewController {
      *  2)  Use of Control Structures:
      *          Requirement Met: Employ loops for iterating through movie data and conditional statements for filtering search results based on user input.
      *          Implementation: The function uses conditional statements to check the validity of the user input and to handle the output based on whether the search results are empty or not. It also uses a loop (while ($movie = pg_fetch_assoc($result))) to iterate through each movie in the result set and display its details.
-     *  3) Use of Built-in Functions:   
+     *  3) Use of Built-in Functions:
      *          trim() to remove whitespace from the user input.
      *          preg_match() to validate the input against a specific pattern.
      *          htmlspecialchars() to safely encode HTML special characters from the database output, preventing XSS (Cross-Site Scripting) attacks when displaying movie titles and descriptions.
@@ -101,25 +126,25 @@ class MovieReviewController {
      *          Implementation: The search functionality is triggered by retrieving data sent via $_GET ($query = $this->input['query'] ?? '';
      *  5) Regular Expression:
      *          Requirement Met: Utilize regular expressions to validate and sanitize search inputs.
-     *          Implementation: The function uses a regular expression (preg_match("/^[a-zA-Z0-9\s]*$/", $query)) to ensure that the search input only contains alphanumeric characters and spaces. 
+     *          Implementation: The function uses a regular expression (preg_match("/^[a-zA-Z0-9\s]*$/", $query)) to ensure that the search input only contains alphanumeric characters and spaces.
      *  6) Secure Database Interaction:
      *           By preparing the SQL query with placeholders and executing it with actual parameters (pg_prepare and pg_execute), the function enhances security by separating data handling from SQL code execution, thereby preventing SQL injection attacks.
-     */  
+     */
     private function searchMovies() {
-    
+
         $query = isset($_GET['query']) ? $_GET['query'] : '';  // Get the search query from input, defaulting to an empty string if not set
         $query = trim($query);  // Trim whitespace
-        
-        
+
+
         // Validate the input
         if (!preg_match("/^[a-zA-Z0-9\s]*$/", $query)) {
-            
+
             $_SESSION['search_results'] = "Invalid input. Only alphanumeric characters and spaces are allowed.";
             // include("/students/xdq9qa/students/xdq9qa/private/sprint3/templates/Search.php");
             include("/opt/src/sprint3/templates/Search.php");
             return;
         }
-    
+
         // $searchTerm = '%' . $query . '%';
         // echo $searchTerm;
         // SQL query
